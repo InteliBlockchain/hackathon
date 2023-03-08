acc7415b0c99bd7f1231dbcd9b42259d59b2b34c
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-03-07 22:05:44 -0300

    fix(Q&A): added alt text

--- components/Q&AInput.tsx
+++ components/Q&AInput.tsx
@@ -55,7 +55,7 @@ export const QeAInput = ({ name, description }: {
                 <div className="flex flex-col h-full ml-4 mr-4 mt-4">
                     <div className="flex flex-row w-full justify-between align-center">
                         <label className="font-normal text-sm md:text-xl">{name}</label>
-                        <button className="bg-transparent h-5" onClick={open}><Image src = {seta} width={20} height={20}></Image></button>
+                        <button className="bg-transparent h-5" onClick={open}><Image alt={"seta"} src={seta} width={20} height={20}></Image></button>
                     </div>
                     <div className="mt-6">
                         <label className="font-extrathin text-md">{description}</label>    
@@ -68,7 +68,7 @@ export const QeAInput = ({ name, description }: {
             <div id="box" className="w-full md:w-10/12 h-14 rounded-lg bg-[#2C2C2C]">
                 <div className="flex justify-between h-full items-center ml-4 mr-4">
                     <label className="font-normal text-xl">{name}</label>
-                    <button className="bg-transparent" onClick={open}><Image src = {seta} width={20} height={20}></Image></button>                                
+                    <button className="bg-transparent" onClick={open}><Image alt={"seta"} src={seta} width={20} height={20}></Image></button>                                
                 </div>
             </div>
         )
