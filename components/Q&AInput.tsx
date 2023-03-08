4e12063e9f2d1d3a0be862ca767437bcfa8aa25c
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-03-07 23:45:47 -0300

    fix: arrow animation and grid applied for q&a questions

--- components/Q&AInput.tsx
+++ components/Q&AInput.tsx
@@ -4,73 +4,79 @@ import Image from "next/image";
 
 import close from "../assets/x.svg"
 
-import seta from "../assets/Seta.png"
+import seta from "../assets/Seta.svg"
 
-export const QeAInput = ({ name, description }: {
-    name: string;
-    description: string;
+export const QeAInput = ({ id, question, answer }: {
+    id: number;
+    question: string;
+    answer: string;
 }) => {
 
-    const [ isOpen, setIsOpen ] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(false);
 
     const open = () => {
-        const box = document.getElementById("box");
+        const box = document.getElementById("box" + id);
+        const arrow = document.getElementById("seta" + id);
 
         //Animate box height when opening
-        if(!isOpen) {
-            box?.animate([
-                { height: "3.5rem" },
-                { height: "12rem" }
-            ], {
-                duration: 800,
-                easing: "ease-in-out",
-                fill: "forwards"
-            })
-        } else {
-            box?.animate([
-                { height: "12rem" },
-                { height: "3.5rem" }
-            ], {
-                duration: 800,
-                easing: "ease-in-out",
-                fill: "forwards"
-            })
-        }
-
         if (!isOpen) {
-            setTimeout(() => {
-                setIsOpen(!isOpen);
-            }, 200);
+            box?.animate([
+                { height: "0" },
+                { "min-height": "3rem", height: "auto" }
+            ], {
+                duration: 800,
+                easing: "ease-in-out",
+                fill: "forwards"
+            })
+
+            arrow?.animate([
+                { transform: "rotate(0deg)" },
+                { transform: "rotate(90deg)" }
+            ], {
+                duration: 600,
+                easing: "ease-in-out",
+                fill: "forwards"
+            })
         } else {
-            setIsOpen(!isOpen);
+            box?.animate([
+                { height: "0" },
+                { "min-height": "3rem", height: "auto" }
+            ], {
+                duration: 800,
+                easing: "ease-in-out",
+                fill: "forwards"
+            })
+
+            arrow?.animate([
+                { transform: "rotate(90deg)" },
+                { transform: "rotate(0deg)" }
+            ], {
+                duration: 600,
+                easing: "ease-in-out",
+                fill: "forwards"
+            })
         }
+
+        setTimeout(() => {
+            setIsOpen(!isOpen);
+        }, 200);
     }
 
-
-    
-
-    if(isOpen) {
-        return (
-            <div id="box" className="w-full md:w-10/12 h-48 rounded-lg bg-[#2C2C2C]">
-                <div className="flex flex-col h-full ml-4 mr-4 mt-4">
-                    <div className="flex flex-row w-full justify-between align-center">
-                        <label className="font-normal text-sm md:text-xl">{name}</label>
-                        <button className="bg-transparent h-5" onClick={open}><Image alt={"seta"} src={seta} width={20} height={20}></Image></button>
+    return (
+        <div onClick={open} className="w-full md:w-f text-xl h-auto p-4 rounded-lg bg-[#2C2C2C]">
+            <div className="flex flex-col h-full">
+                <div className="flex flex-row w-full justify-between align-center">
+                    <label className="font-medium text-xl h-auto">{question}</label>
+                    <button className="bg-transparent">
+                        <Image alt={"seta"} id={`seta${id}`} src={seta} width={14} height={14}></Image>
+                    </button>
+                </div>
+                {isOpen ? (
+                    <div id={`box${id}`} className="mt-4 transition-all duration-200 ease-in-out h-auto">
+                        <p className="text-base opacity-80 font-extrathin">{answer}</p>
                     </div>
-                    <div className="mt-6">
-                        <label className="font-extrathin text-md">{description}</label>    
-                    </div>            
-                </div>
+                ) : null}
             </div>
-        )
-    } else {
-        return (
-            <div id="box" className="w-full md:w-10/12 h-14 rounded-lg bg-[#2C2C2C]">
-                <div className="flex justify-between h-full items-center ml-4 mr-4">
-                    <label className="font-normal text-xl">{name}</label>
-                    <button className="bg-transparent" onClick={open}><Image alt={"seta"} src={seta} width={20} height={20}></Image></button>                                
-                </div>
-            </div>
-        )
-    }
+        </div>
+    )
 }
