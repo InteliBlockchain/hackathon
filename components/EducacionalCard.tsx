c68b5c18907bfd8ee83f0aee756a2c29cf975bcf
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2023-03-08 22:26:20 -0300

    feat(educacional): added link support

--- components/EducacionalCard.tsx
+++ components/EducacionalCard.tsx
@@ -1,5 +1,6 @@
 import { type } from "os"
 import { useEffect, useState } from "react"
+import Link from "next/link"
 
 type Props = {
     title: string,
@@ -34,9 +35,15 @@ const EducacionalCard = ({title, text, level, type}: Props) => {
     <div className="m-2 border-2 flex flex-col shadow-purple1 shadow rounded-lg border-purple1 w-5/6 h-auto items-center">
     <p className="p-3 font-medium text-xl">{title}</p>
     <p className="text-sm p-2">{text}</p>
-    <div className="flex">
+    <div className="flex p-2">
         <p className="mr-32">{type}</p>
         <p className={levelStyle}>{levelText}</p>
+    </div>
+
+    <div className="text-center p-2 h-12 hover:text-black transition-all text-purple1 hover:bg-purple1 rounded-lg border-t-2 border-purple1 w-full">
+    <Link href={"https://google.com"}>
+        <p className="text-lg"> Acessar</p>
+    </Link>
     </div>
 </div>
     )
