db131a4482a44615a35ff7e2d7fa00ace5803f32
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-03-20 23:11:55 -0300

    fix: api link on next.config.js env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   // reactStrictMode: true,
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_allow_subscriptions: true
+    NEXT_PUBLIC_allow_subscriptions: true,
+    NEXT_PUBLIC_API_URL: 'https://api.inteliblockchain.co'
   }
 }
 
