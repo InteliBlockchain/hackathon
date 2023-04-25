c9705981b709af8ce000640b32deff79f1de1c0c
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-04-24 21:58:20 -0300

    fix: maintanance mode

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   // reactStrictMode: true,
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_allow_subscriptions: true,
     NEXT_PUBLIC_API_URL: 'https://api.inteliblockchain.co',
     NEXT_PUBLIC_ALLOW_SUBSCRIPTIONS: true,
     NEXT_PUBLIC_JWT_TOKEN_VALIDATION_FRONT: "fda185375967e0569363a5f061f0e1ae",
-    NEXT_PUBLIC_MAINTANANCE_MODE: false,
-    NEXT_PUBLIC_SUBSCRIPTIONS_ENDED: false
+    NEXT_PUBLIC_allow_subscriptions: false, // true
+    NEXT_PUBLIC_MAINTANANCE_MODE: true, // false
+    NEXT_PUBLIC_SUBSCRIPTIONS_ENDED: true // false
   }
 }
 
