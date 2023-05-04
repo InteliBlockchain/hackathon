96e209007ddaea3a5ee4eafe2e477507299f15a2
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-05-04 12:47:31 +0000

    fix: maintanance mode disabled

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: 'https://hackathon-back.herokuapp.com',
     NEXT_PUBLIC_ALLOW_SUBSCRIPTIONS: true,
     NEXT_PUBLIC_JWT_TOKEN_VALIDATION_FRONT: "fda185375967e0569363a5f061f0e1ae",
-    NEXT_PUBLIC_MAINTANANCE_MODE: true,
+    NEXT_PUBLIC_MAINTANANCE_MODE: false,
     NEXT_PUBLIC_SUBSCRIPTIONS_ENDED: false,
   }
 }
